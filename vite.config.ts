742e0d53dba8faf67b538997fbd0107247b14081
lh <1767359165@qq.com>
2024-07-12 17:01:25 +0800

    UnoCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default ({ command, mode }) => {
   return defineConfig({
-    envDir: './ env',
-    plugins: [uni()],
+    envDir: './env',
+    plugins: [uni(), UnoCSS()],
   })
 }
