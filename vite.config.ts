e08370fee7a95247274f6989d43c8526d187619d
LHNB521 <1767359165@qq.com>
2024-07-12 21:26:47 +0800

    配置环境

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,58 @@
 import { UserConfig, ConfigEnv, loadEnv, defineConfig } from 'vite'
+import path from 'node:path'
 
 import createPlugins from './vites'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }: ConfigEnv): UserConfig => {
-  const env = loadEnv(mode, process.cwd())
-  const { UNI_PLATFORM } = process.env
+  const env = loadEnv(mode, path.resolve(process.cwd(), 'env'))
+  // 获取环境变量h5 小程序
+  // const { UNI_PLATFORM } = process.env
+  const {
+    VITE_APP_PORT,
+    VITE_SERVER_BASEURL,
+    VITE_DELETE_CONSOLE,
+    VITE_SHOW_SOURCEMAP,
+    VITE_APP_PROXY,
+    VITE_APP_PROXY_PREFIX,
+  } = env
 
   return {
     envDir: './env',
     plugins: createPlugins(env, command === 'build'),
+    resolve: {
+      alias: {
+        '@': path.join(process.cwd(), './src'),
+        '@img': path.join(process.cwd(), './src/static/images'),
+      },
+    },
+    server: {
+      host: '0.0.0.0',
+      hmr: true,
+      port: Number.parseInt(VITE_APP_PORT, 10),
+      // 仅 H5 端生效，其他端不生效（其他端走build，不走devServer)
+      proxy: JSON.parse(VITE_APP_PROXY)
+        ? {
+            [VITE_APP_PROXY_PREFIX]: {
+              target: VITE_SERVER_BASEURL,
+              changeOrigin: true,
+              rewrite: (path) => path.replace(new RegExp(`^${VITE_APP_PROXY_PREFIX}`), ''),
+            },
+          }
+        : undefined,
+    },
+    build: {
+      // 方便非h5端调试
+      sourcemap: VITE_SHOW_SOURCEMAP === 'true', // 默认是false
+      target: 'es6',
+      // 开发环境不用压缩
+      minify: mode === 'development' ? false : 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: VITE_DELETE_CONSOLE === 'true',
+          drop_debugger: true,
+        },
+      },
+    },
   }
 })
